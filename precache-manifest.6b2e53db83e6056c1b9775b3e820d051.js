e32c193c8be99c0e8001fb0410e8094ac807c9ca
Alvaro Padilla <varoboss.ap@gmail.com>
2020-06-14 21:18:14 -0500

    Updates

---
+++ precache-manifest.6b2e53db83e6056c1b9775b3e820d051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9072874976e71e601f27f32d4b54664",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "22fcf5d1b7263d8b1bd3",
+    "url": "/react-tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cfc1a60f95ad5334208",
+    "url": "/react-tutorial/static/js/2.0da6d042.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0da6d042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fcf5d1b7263d8b1bd3",
+    "url": "/react-tutorial/static/js/main.30784fed.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
